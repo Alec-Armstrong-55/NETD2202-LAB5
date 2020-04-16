72aa861c5740050a80a7cf93ff864641f6521a21
Alec-Armstrong-55 <60987510+Alec-Armstrong-55@users.noreply.github.com>
2020-04-16 12:21:46 -0400

    Add files via upload

---
+++ NETD2202LAB5/Lab5.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmArmstrongEditor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.mnuMenu = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileNew = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileSaveAs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEdit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEditCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEditCut = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEditPaste = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelpAbout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.txtEditor = New System.Windows.Forms.TextBox()
+        Me.opdOpen = New System.Windows.Forms.OpenFileDialog()
+        Me.sfdSaveAs = New System.Windows.Forms.SaveFileDialog()
+        Me.ttpMain = New System.Windows.Forms.ToolTip(Me.components)
+        Me.mnuMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'mnuMenu
+        '
+        Me.mnuMenu.ImageScalingSize = New System.Drawing.Size(40, 40)
+        Me.mnuMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuEdit, Me.mnuHelp})
+        Me.mnuMenu.Location = New System.Drawing.Point(0, 0)
+        Me.mnuMenu.Name = "mnuMenu"
+        Me.mnuMenu.Size = New System.Drawing.Size(2040, 49)
+        Me.mnuMenu.TabIndex = 0
+        Me.mnuMenu.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFileNew, Me.mnuFileOpen, Me.mnuFileSave, Me.mnuFileSaveAs, Me.mnuFileExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(87, 48)
+        Me.mnuFile.Text = "&File"
+        Me.mnuFile.ToolTipText = "File manipulation."
+        '
+        'mnuFileNew
+        '
+        Me.mnuFileNew.Name = "mnuFileNew"
+        Me.mnuFileNew.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.mnuFileNew.Size = New System.Drawing.Size(448, 54)
+        Me.mnuFileNew.Text = "&New"
+        Me.mnuFileNew.ToolTipText = "Create a new blank file."
+        '
+        'mnuFileOpen
+        '
+        Me.mnuFileOpen.Name = "mnuFileOpen"
+        Me.mnuFileOpen.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.mnuFileOpen.Size = New System.Drawing.Size(448, 54)
+        Me.mnuFileOpen.Text = "&Open"
+        Me.mnuFileOpen.ToolTipText = "Open an existing file."
+        '
+        'mnuFileSave
+        '
+        Me.mnuFileSave.Name = "mnuFileSave"
+        Me.mnuFileSave.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.mnuFileSave.Size = New System.Drawing.Size(448, 54)
+        Me.mnuFileSave.Text = "&Save"
+        Me.mnuFileSave.ToolTipText = "Save the current file."
+        '
+        'mnuFileSaveAs
+        '
+        Me.mnuFileSaveAs.Name = "mnuFileSaveAs"
+        Me.mnuFileSaveAs.Size = New System.Drawing.Size(448, 54)
+        Me.mnuFileSaveAs.Text = "S&ave As"
+        Me.mnuFileSaveAs.ToolTipText = "Save the current file as."
+        '
+        'mnuFileExit
+        '
+        Me.mnuFileExit.Name = "mnuFileExit"
+        Me.mnuFileExit.Size = New System.Drawing.Size(448, 54)
+        Me.mnuFileExit.Text = "E&xit"
+        Me.mnuFileExit.ToolTipText = "Exit the program."
+        '
+        'mnuEdit
+        '
+        Me.mnuEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuEditCopy, Me.mnuEditCut, Me.mnuEditPaste})
+        Me.mnuEdit.Name = "mnuEdit"
+        Me.mnuEdit.Size = New System.Drawing.Size(92, 45)
+        Me.mnuEdit.Text = "&Edit"
+        Me.mnuEdit.ToolTipText = "Edit the text."
+        '
+        'mnuEditCopy
+        '
+        Me.mnuEditCopy.Name = "mnuEditCopy"
+        Me.mnuEditCopy.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.mnuEditCopy.Size = New System.Drawing.Size(448, 54)
+        Me.mnuEditCopy.Text = "&Copy"
+        Me.mnuEditCopy.ToolTipText = "Copy the selected text."
+        '
+        'mnuEditCut
+        '
+        Me.mnuEditCut.Name = "mnuEditCut"
+        Me.mnuEditCut.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.mnuEditCut.Size = New System.Drawing.Size(448, 54)
+        Me.mnuEditCut.Text = "Cu&t"
+        Me.mnuEditCut.ToolTipText = "Cut the slected text."
+        '
+        'mnuEditPaste
+        '
+        Me.mnuEditPaste.Name = "mnuEditPaste"
+        Me.mnuEditPaste.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
+        Me.mnuEditPaste.Size = New System.Drawing.Size(448, 54)
+        Me.mnuEditPaste.Text = "&Paste"
+        Me.mnuEditPaste.ToolTipText = "Paste text in the clipboard."
+        '
+        'mnuHelp
+        '
+        Me.mnuHelp.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuHelpAbout})
+        Me.mnuHelp.Name = "mnuHelp"
+        Me.mnuHelp.Size = New System.Drawing.Size(104, 45)
+        Me.mnuHelp.Text = "&Help"
+        Me.mnuHelp.ToolTipText = "Help Options."
+        '
+        'mnuHelpAbout
+        '
+        Me.mnuHelpAbout.Name = "mnuHelpAbout"
+        Me.mnuHelpAbout.ShortcutKeys = System.Windows.Forms.Keys.F1
+        Me.mnuHelpAbout.Size = New System.Drawing.Size(448, 54)
+        Me.mnuHelpAbout.Text = "&About"
+        Me.mnuHelpAbout.ToolTipText = "About the application."
+        '
+        'txtEditor
+        '
+        Me.txtEditor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtEditor.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtEditor.Location = New System.Drawing.Point(0, 49)
+        Me.txtEditor.Multiline = True
+        Me.txtEditor.Name = "txtEditor"
+        Me.txtEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtEditor.Size = New System.Drawing.Size(2040, 966)
+        Me.txtEditor.TabIndex = 1
+        Me.txtEditor.TabStop = False
+        Me.ttpMain.SetToolTip(Me.txtEditor, "Editor.")
+        '
+        'frmArmstrongEditor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(2040, 1015)
+        Me.Controls.Add(Me.txtEditor)
+        Me.Controls.Add(Me.mnuMenu)
+        Me.MainMenuStrip = Me.mnuMenu
+        Me.Name = "frmArmstrongEditor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Armstrong Editor"
+        Me.ttpMain.SetToolTip(Me, "Text editor application.")
+        Me.mnuMenu.ResumeLayout(False)
+        Me.mnuMenu.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents mnuMenu As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuFileNew As ToolStripMenuItem
+    Friend WithEvents mnuFileOpen As ToolStripMenuItem
+    Friend WithEvents mnuFileSave As ToolStripMenuItem
+    Friend WithEvents mnuFileSaveAs As ToolStripMenuItem
+    Friend WithEvents mnuFileExit As ToolStripMenuItem
+    Friend WithEvents mnuEdit As ToolStripMenuItem
+    Friend WithEvents mnuEditCopy As ToolStripMenuItem
+    Friend WithEvents mnuEditCut As ToolStripMenuItem
+    Friend WithEvents mnuEditPaste As ToolStripMenuItem
+    Friend WithEvents mnuHelp As ToolStripMenuItem
+    Friend WithEvents mnuHelpAbout As ToolStripMenuItem
+    Friend WithEvents txtEditor As TextBox
+    Friend WithEvents opdOpen As OpenFileDialog
+    Friend WithEvents sfdSaveAs As SaveFileDialog
+    Friend WithEvents ttpMain As ToolTip
+End Class
---
+++ NETD2202LAB5/Lab5.vb
@@ -0,0 +1,238 @@
+﻿' Author: Alec Armstrong 100617553
+' Date: April 2, 2020
+' Description:
+'   This program is a simple text editor.
+'   A user can enter text into the textbox, and resize and inimize the form.
+'   Basic functionality to Save, Open, Exit and create a new form, also some clipboard
+'   functionality in Copy, Paste and Cut. About info is also available.
+
+Option Strict On
+
+Imports System.IO
+
+Public Class frmArmstrongEditor
+
+    ' Variable for keeping track of the file path
+    Dim filePath As String = String.Empty
+
+    ' Checks if the cut function was last used before the paste.
+    Dim isCut As Boolean = False
+
+    ''' <summary>
+    ''' When the application is opened, set to defaults.
+    ''' </summary>
+    Private Sub frmArmstrongEditor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        SetDefaults()
+
+    End Sub
+
+#Region "File Event Handlers"
+
+    ''' <summary>
+    ''' Opens a new file after checking if the user wants to save the current file, setting to defaults.
+    ''' </summary>
+    Private Sub mnuFileNew_Click(sender As Object, e As EventArgs) Handles mnuFileNew.Click
+
+        SetDefaults()
+
+    End Sub
+
+    ''' <summary>
+    ''' Opens a previously created text document.
+    ''' </summary>
+    Private Sub mnuFileOpen_Click(sender As Object, e As EventArgs) Handles mnuFileOpen.Click
+
+        ' objects used to open and read a file.
+        Dim openFile As FileStream
+        Dim fileReader As StreamReader
+
+        ' If a file is selected.
+        If opdOpen.ShowDialog() = DialogResult.OK Then
+
+            ' Make the file path the chosen one.
+            filePath = opdOpen.FileName
+
+            ' Update the form title.
+            UpdateFormTitle()
+
+            ' Open the chosen file and read it.
+            openFile = New FileStream(filePath, FileMode.Open, FileAccess.Read)
+            fileReader = New StreamReader(openFile)
+            txtEditor.Text = fileReader.ReadToEnd
+
+            ' Close the file reader.
+            fileReader.Close()
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Saves the document currently open or calls upon the save as option.
+    ''' </summary>
+    Public Sub mnuFileSave_Click(sender As Object, e As EventArgs) Handles mnuFileSave.Click
+
+        If filePath = String.Empty Then
+
+            ' Use Save As.
+            mnuFileSaveAs_Click(sender, e)
+
+        Else
+
+            ' Save the file.
+            SaveTextFile(filePath)
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Saves the document currently open to the specified locationin memory.
+    ''' </summary>
+    Private Sub mnuFileSaveAs_Click(sender As Object, e As EventArgs) Handles mnuFileSaveAs.Click
+
+        ' Sets the filter for the save dialog.
+        sfdSaveAs.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+
+        ' If the user selects a file.
+        If sfdSaveAs.ShowDialog() = DialogResult.OK Then
+
+            ' Set the new file path
+            filePath = sfdSaveAs.FileName
+
+            ' Update the form title
+            UpdateFormTitle()
+
+            ' Save the file.
+            SaveTextFile(filePath)
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Exit the application.
+    ''' </summary>
+    Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles mnuFileExit.Click
+
+        Me.Close()
+
+    End Sub
+
+#End Region
+
+#Region "Edit Event Handlers"
+
+    ''' <summary>
+    ''' Copy the selected text.
+    ''' </summary>
+    Private Sub mnuEditCopy_Click(sender As Object, e As EventArgs) Handles mnuEditCopy.Click
+
+        If Not txtEditor.SelectedText = String.Empty Then
+
+            Clipboard.SetText(txtEditor.SelectedText)
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Cuts the selected text.
+    ''' </summary>
+    Private Sub mnuEditCut_Click(sender As Object, e As EventArgs) Handles mnuEditCut.Click
+
+        If Not txtEditor.SelectedText = String.Empty Then
+
+            Clipboard.SetText(txtEditor.SelectedText)
+            txtEditor.SelectedText = String.Empty
+
+            isCut = True
+
+        End If
+
+
+    End Sub
+
+    ''' <summary>
+    ''' Pastes the clipboard.
+    ''' </summary>
+    Private Sub mnuEditPaste_Click(sender As Object, e As EventArgs) Handles mnuEditPaste.Click
+
+        txtEditor.Paste(Clipboard.GetText())
+
+        If isCut = True Then
+
+            Clipboard.Clear()
+
+            isCut = False
+
+        End If
+
+    End Sub
+
+#End Region
+
+#Region "Help Event Handler"
+
+    ''' <summary>
+    ''' Displays info on the form.
+    ''' </summary>
+    Private Sub mnuHelpAbout_Click(sender As Object, e As EventArgs) Handles mnuHelpAbout.Click
+
+        MessageBox.Show("Lab 5" & vbCrLf & vbCrLf & "Alec Armstrong 100617553" & vbCrLf & vbCrLf & "02-04-2020")
+
+    End Sub
+
+#End Region
+
+#Region "Methods"
+
+    ''' <summary>
+    ''' Set the defaults of the form.
+    ''' </summary>
+    Friend Sub SetDefaults()
+
+        ' Clear anything inside the text box and put focus on the Editor.
+        txtEditor.Text = String.Empty
+        txtEditor.Focus()
+
+        ' Unset the saved file path and update the form title
+        filePath = String.Empty
+        UpdateFormTitle()
+
+    End Sub
+
+    ''' <summary>
+    ''' Saves the file that is passed into it.
+    ''' </summary>
+    ''' <param name="path">A full file path as a string</param>
+    Friend Sub SaveTextFile(path As String)
+
+        Dim fileToAccess As New FileStream(path, FileMode.Create, FileAccess.Write)
+        Dim writer As New StreamWriter(fileToAccess)
+
+        writer.Write(txtEditor.Text)
+
+        writer.Close()
+
+    End Sub
+
+    ''' <summary>
+    ''' Updates the form's title text to include the open file path.
+    ''' </summary>
+    Friend Sub UpdateFormTitle()
+
+        Me.Text = "Armstrong Editor"
+
+        If Not filePath = String.Empty Then
+
+            Me.Text &= " - " & filePath
+
+        End If
+
+    End Sub
+
+#End Region
+
+End Class
